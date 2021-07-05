83eefedde5d477ff2410ac7810473e7cd0d36673
Peter Roto <peterdroto@gmail.com>
2021-07-05 13:44:57 -0500

    init commit

---
+++ models/Category.js
@@ -0,0 +1,26 @@
+const { DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+
+
+const Category = sequelize.define('Category', {
+
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    category_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: 'category',
+});
+
+module.exports = Category;
