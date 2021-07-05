3935272823c6d9e36136eed34e75710330acfbab
Peter Roto <peterdroto@gmail.com>
2021-07-05 14:34:04 -0500

    Finished Product Routes, Added Get and Get by ID to Category

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,7 +30,34 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     // find a single product by its `id`
     // be sure to include its associated Category and Tag data
+    Product.findOne({
+            where: {
+                id: req.params.id
+            },
+            attributes: ['id', 'product_name', 'price', 'stock'],
+            include: [{
+                    model: Category,
+                    attributes: ['category_name']
+                },
+                {
+                    model: Tag,
+                    attributes: ['tag_name']
+                }
+            ]
+        })
+        .then(dbProductData => {
+            if (!dbProductData) {
+                res.status(404).json({ message: 'Swing and a miss. . . try a different ID' });
+                return;
+            }
+            res.json(dbProductData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
+
 
 // create new product
 router.post('/', (req, res) => {
